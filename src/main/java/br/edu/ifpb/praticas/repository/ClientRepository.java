8206225389e59a87ef63ab9eb129b299fbb62de2
Dijalma Silva <dijalmacz@gmail.com>
2017-04-25 21:26:54 -0300

    Alter repository names

--- src/main/java/br/edu/ifpb/praticas/repository/ClientRepository.java
+++ src/main/java/br/edu/ifpb/praticas/repository/ClientRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
  * Created by <a href="http://dijalmasilva.github.io/" target="_blank">Dijalma Silva</a> on 17/02/17 - 10:51
  */
 @Repository
-public interface ClienteRepository extends CrudRepository<Client, Long>{
+public interface ClientRepository extends CrudRepository<Client, Long>{
 
     Client findByUser(User user);
 }
--- src/main/java/br/edu/ifpb/praticas/repository/ProviderRepository.java
+++ src/main/java/br/edu/ifpb/praticas/repository/ProviderRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.praticas.repository;
 
 import br.edu.ifpb.praticas.model.Provider;
+import br.edu.ifpb.praticas.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
  * Created by <a href="http://dijalmasilva.github.io/" target="_blank">Dijalma Silva</a> on 17/02/17 - 10:51
  */
 @Repository
-public interface PrestadorRepository extends CrudRepository<Provider, Long>{
+public interface ProviderRepository extends CrudRepository<Provider, Long>{
+
+    Provider findByUser(User user);
 }

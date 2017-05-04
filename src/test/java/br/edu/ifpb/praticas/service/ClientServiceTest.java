4e83a6e83d6b15ed2d3b40900b6e85be14f86648
Dijalma Silva <dijalmacz@gmail.com>
2017-05-03 21:04:21 -0300

    Testes em Client funcionando

--- src/test/java/br/edu/ifpb/praticas/service/ClientServiceTest.java
+++ src/test/java/br/edu/ifpb/praticas/service/ClientServiceTest.java
@@ -1,13 +1,23 @@
 package br.edu.ifpb.praticas.service;
 
 import br.edu.ifpb.praticas.Exception.ClientException;
+import br.edu.ifpb.praticas.enums.StatusEnum;
+import br.edu.ifpb.praticas.model.Client;
+import br.edu.ifpb.praticas.model.PlaceOfCare;
+import br.edu.ifpb.praticas.model.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by <a href="http://dijalmasilva.github.io" target="_blank">dijalma</a> on 25/04/17.
@@ -17,28 +27,65 @@ import org.springframework.test.context.junit4.SpringRunner;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class ClientServiceTest {
 
+    @Autowired
+    private ClientService clientService;
+    private Client client;
+
     @Before
     public void setUp() {
+        client = new Client();
+        PlaceOfCare place = new PlaceOfCare();
+        place.setCity("Cajazeiras");
+        place.setDistrict("Casas Populares");
+        client.setPlaceOfCare(place);
+        client.setStatus(StatusEnum.ATIVO);
+        User user = new User();
+        user.setUsername("dijalmaf");
+        user.setPassword("1233ddd");
+        client.setId(1001L);
+        client.setUser(user);
+        client.setCpf("685.347.391-45");
+        client.setName("Dijalma");
+        client.setLastName("Silva");
+        client.setEmail("dijalmacz@gmail.com");
+        client.setTelephone("(83) 99900-5077");
     }
 
     @Test
     public void save() throws ClientException {
+        Client save = clientService.save(client);
+        assertNotNull(save);
     }
 
     @Test
     public void edit() throws ClientException {
+        Client finded = clientService.findById(1L);
+        finded.setName("Chanaína");
+        finded.setCpf("926.729.717-14");
+        Client edit = clientService.edit(1L, finded);
+        assertEquals("Chanaína", edit.getName());
     }
 
     @Test
     public void findById() throws ClientException {
+        Client finded = clientService.findById(2L);
+        assertNotNull(finded.getId());
     }
 
     @Test
     public void findAll() throws ClientException {
+        assertEquals(50, clientService.findAll().size());
     }
 
     @Test
     public void delete() throws ClientException {
+        client.setId(1022L);
+        client.setName("Joinhaa");
+        client.getUser().setUsername("joinhaa");
+        clientService.save(client);
+        clientService.delete(1022L);
+        Client finded = clientService.findById(1022L);
+        assertNull(finded);
     }
 
 }
--- src/test/java/br/edu/ifpb/praticas/service/ProviderServiceTest.java
+++ src/test/java/br/edu/ifpb/praticas/service/ProviderServiceTest.java
@@ -1,16 +1,51 @@
 package br.edu.ifpb.praticas.service;
 
 import br.edu.ifpb.praticas.Exception.ProviderException;
+import br.edu.ifpb.praticas.enums.StatusEnum;
+import br.edu.ifpb.praticas.model.Provider;
+import br.edu.ifpb.praticas.model.User;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by <a href="http://dijalmasilva.github.io" target="_blank">dijalma</a> on 25/04/17.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class ProviderServiceTest {
+
+    @Autowired
+    private ProviderService service;
+    private Provider provider;
+
+    @Before
+    public void setUp() throws Exception {
+        provider = new Provider();
+        provider.setCpf("685.347.391-45");
+        provider.setStatus(StatusEnum.ATIVO);
+        provider.setEmail("teste@gmail.com");
+        provider.setName("Teste");
+        provider.setTelephone("(83) 99999-9999");
+        provider.setLastName("SobreTeste");
+        provider.setId(4012L);
+        User user = new User();
+        user.setUsername("teste");
+        user.setPassword("teste123");
+        provider.setUser(user);
+    }
+
     @Test
     public void save() throws ProviderException {
+        
     }
 
     @Test
